fc14f3325e6296054aca3bba11be64faff9f6adc
steve <steve@DESKTOP-RFPL45I>
2021-06-06 18:16:49 -0400

    v1 done

--- portfoliosite/src/components/Leadership.js
+++ portfoliosite/src/components/Leadership.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChalkboardTeacher } from '@fortawesome/free-solid-svg-icons'
-import { Fade, Bounce } from "react-reveal";
+import { Fade } from "react-reveal";
 import ucf from '../assets/UCFlogo.png';
 import magicmeds from '../assets/magicmeds.png';
 import magicmedsapp from '../assets/MagicMedsApp.jpg';
